932f227690de65c1859708567bd44d42b98a0c9b
D34dMan <D34dMan@751698.no-reply.drupal.org>
2015-02-15 19:31:12 +0100

    Providing field name in wrapper for pm list formatter.

--- pm-list-default.tpl.php
+++ pm-list-default.tpl.php
@@ -1,3 +1,3 @@
-<div class="pm-list-default-wrapper">
+<div class="pm-list-default-wrapper pm-list-field-name-<?php echo $field_name ?>">
   <span class="pm-icon pm-list-default-icon-<?php echo str_replace(' ', '_', $key); ?>"></span>&nbsp;<span class="value"><?php echo $value; ?></span>
 </div>
