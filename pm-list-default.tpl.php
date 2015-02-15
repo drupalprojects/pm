4f01331989a7b14d07f1547a2a411f9ebde5cdf2
D34dMan <D34dMan@751698.no-reply.drupal.org>
2015-02-15 03:01:57 +0100

    Issue #2360363 by D34dMan: Bugfix, spaces in the keyname is replaced by "_" (underscore) to be properly recognized by css styles.

--- pm-list-default.tpl.php
+++ pm-list-default.tpl.php
@@ -1,3 +1,3 @@
 <div class="pm-list-default-wrapper">
-  <span class="pm-icon pm-list-default-icon-<?php echo $key ?>"></span>&nbsp;<span class="value"><?php echo $value; ?></span>
+  <span class="pm-icon pm-list-default-icon-<?php echo str_replace(' ', '_', $key); ?>"></span>&nbsp;<span class="value"><?php echo $value; ?></span>
 </div>
