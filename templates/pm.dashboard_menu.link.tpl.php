a39062465f5f841209c5eda2e5597f743a07822b
D34dMan <shibinkidd@gmail.com>
2016-04-17 16:18:23 +0200

    Issue #2506299: Changing theme function to generate links to use templates

---
+++ templates/pm.dashboard_menu.link.tpl.php
@@ -0,0 +1,15 @@
+<dt class="pmcomponent">
+  <?php if ($icon): ?>
+    <?php echo $icon; ?>
+  <?php endif ?>
+  <a href="<?php echo $path; ?>">
+    <?php echo $title; ?>
+  </a>
+  <?php if ($extra_link): ?>
+    <a href="<?php echo $extra_link; ?>" title="Add">
+      <?php if ($extra_link_icon): ?>
+        <?php echo $extra_link_icon; ?>
+      <?php endif ?>
+    </a>
+  <?php endif ?>
+</dt>
