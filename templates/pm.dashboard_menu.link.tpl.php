72738eb2a95864441253f2a356545d15619874af
D34dMan <shibinkidd@gmail.com>
2016-04-18 00:03:37 +0200

    Issue #2506299: Exposed few theming functions for creating icon

--- templates/pm.dashboard_menu.link.tpl.php
+++ templates/pm.dashboard_menu.link.tpl.php
@@ -2,9 +2,7 @@
   <?php if ($icon): ?>
     <?php echo $icon; ?>
   <?php endif ?>
-  <a href="<?php echo $path; ?>">
-    <?php echo $title; ?>
-  </a>
+  <a href="<?php echo $path; ?>"><?php echo $title; ?></a>
   <?php if ($extra_link): ?>
     <a href="<?php echo $extra_link; ?>" title="Add">
       <?php if ($extra_link_icon): ?>
